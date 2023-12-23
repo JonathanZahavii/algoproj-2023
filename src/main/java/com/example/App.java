a3e3d7081bbb752fcad6cca65e856f399436db3e
jjonnyzz <xd.JZahavi@idf.il>
2023-12-23 14:54:44 +0200

    Use InspectAanswer object when returning graph solve

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,10 +4,11 @@ public class App {
 
     public static void main(String[] args) {
         Board initBoard = new Board(15);
-        initBoard.generateSolvableInXMoves(15);
+        initBoard.generateSolvableInXMoves(10);
         // Board initBoard = Board.getBoardFromUser();
         Board goalBoard = new Board(15);
         Graph graph = new Graph();
-        graph.BFS(initBoard, goalBoard);
+        InspectAnswer inspectAnswer = graph.BFS(initBoard, goalBoard);
+        inspectAnswer.print();
     }
 }
