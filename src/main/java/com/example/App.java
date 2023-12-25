7511280ea1fe0c1c94a3f5f249913914f45da1dd
jjonnyzz <xd.JZahavi@idf.il>
2023-12-25 23:32:32 +0200

    Final one for exam

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,15 +4,15 @@ public class App {
 
     public static void main(String[] args) {
         final int RUNS = 5;
-        final int SIZE = 24;
+        final int SIZE = 15;
         final int MOVES = 10;
         final Board GOAL_BOARD = new Board(SIZE);
-        boolean isFromUser = false;
+        final boolean isFromUser = false;
 
         Game gameOnce = new Game(SIZE, MOVES, GOAL_BOARD, isFromUser);
         gameOnce.runOnce("BFS");
 
-        Game gameMultiple = new Game(RUNS, SIZE, MOVES, GOAL_BOARD);
-        gameMultiple.runMultiple();
+        // Game gameMultiple = new Game(RUNS, SIZE, MOVES, GOAL_BOARD);
+        // gameMultiple.runMultiple();
     }
 }
